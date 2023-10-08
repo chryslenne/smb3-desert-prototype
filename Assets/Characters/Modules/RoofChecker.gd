a440f1a30fbe333fd2c43c9d48e16968f01314d2
Nestor Jr. Sirilan <chryslenne.lunarpath@gmail.com>
2023-10-08 17:31:13 -0400

    roof checker update

--- Assets/Characters/Modules/RoofChecker.gd
+++ Assets/Characters/Modules/RoofChecker.gd
@@ -14,13 +14,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	is_roofed = ($ShapeCast2D as ShapeCast2D).get_collision_count() > 0
-#	var space_state = get_world_2d().direct_space_state
-#	# use global coordinates, not local to node
-#	var query = $ShapeCast2D.add_exception()
-##	var query = PhysicsShapeQueryParameters2D.create(
-##		self.global_position, 
-##		self.global_position + Vector2.UP * 1)
-#	query.exclude = exception
-#	var result = space_state.intersect_ray(query)
-#	print(result)
-#	is_roofed = result.size() > 0
